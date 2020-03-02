338086ef2101427268be346d7c0de4e8ca8e07a8
chalst1988 <59086665+chalst1988@users.noreply.github.com>
2020-03-03 00:37:04 +0530

    Add files via upload

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. This Notebook is mainly used for Capstone Project
+
+# 2. Import Pandas library as pd
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# 3. Import numpy library as np
+
+# In[2]:
+
+
+import numpy as np
+
+
+# 4. Print the following the statement: Hello Capstone Project Course!
+
+# In[3]:
+
+
+print('Hello Cpastone Project Course!')
+
+
+# In[ ]:
+
+
+
+
